f759286387b502c9de069cd8f0a516dfb48ac54a
VanessaTinberus <42710894+VanessaTinberus@users.noreply.github.com>
2021-07-11 19:29:27 +0800

    Update heroes.js

--- scripts/heroes.js
+++ scripts/heroes.js
@@ -4,6 +4,10 @@ const header = document.querySelector('header');
 //Grab a reference to the section tag in the HTML file
 const section = document.querySelector('section');
 
+let requestURL = 'https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json';
+
+
+
 //create a request
 let request = new XMLHttpRequest();
 
